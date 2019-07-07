78a87f457cd5fbe55108c7030473af12264c36aa
Evgenii Kazakov <evgenstf@gmail.com>
2019-07-07 16:42:10 +0100

    common: add several assignments

---
+++ variables_typeof/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var name = 'Ivan';
+var age = 13;
+var in_relations = false;
+var spouse = undefined;
+var null_field = null;
+
+
+console.log(typeof (name))
+console.log(typeof (age))
+console.log(typeof (in_relations))
+console.log(typeof (spouse))
+console.log(typeof (null_field))
+
